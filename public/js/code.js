86b21343b24afa47d2460b287fcb2abb54017ea2
aaron <alurui1562@ieselcaminas.org>
2022-01-27 20:10:37 +0100

    create page controller adding bugs works no db

---
+++ public/js/code.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+   let bugTest = {
+        id: 1,
+        description: $("#description")[0].innerHTML,
+        tag: $(".tag")[0].innerHTML,
+        user: "user1"
+   }
+   
+   $("#user").on("click", function() {
+       $.ajax({
+           type: "POST",
+           url : "/bug",
+           data: bugTest
+       }).done((response) => $("#droppable").append(response))
+       .fail((request) => alert("something went wrong " + request.status));
+   });
+
+});

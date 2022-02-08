a311d7764b7dbc78978b4771865f2bf406106c2a
aaron <alurui1562@ieselcaminas.org>
2022-02-08 17:46:06 +0100

    retoques de apariencia

--- public/js/code.js
+++ public/js/code.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         $(".user-container").css({visibility : "visible"});
         $(".info-container").css({visibility : "hidden"}); 
         $(".add-container").css({visibility : "hidden"}); 
+        $(".filter-container").css({visibility : "hidden"}); 
     }); 
     $("#exit-user").on("click", function() {
         $(".user-container").css({visibility : "hidden"}); 
@@ -12,6 +13,7 @@ $(document).ready(function () {
         $(".info-container").css({visibility : "visible"});
         $(".user-container").css({visibility : "hidden"}); 
         $(".add-container").css({visibility : "hidden"}); 
+        $(".filter-container").css({visibility : "hidden"}); 
     }); 
     $("#exit-info").on("click", function() {
         $(".info-container").css({visibility : "hidden"}); 
@@ -21,9 +23,19 @@ $(document).ready(function () {
         $(".add-container").css({visibility : "visible"});
         $(".user-container").css({visibility : "hidden"}); 
         $(".info-container").css({visibility : "hidden"}); 
+        $(".filter-container").css({visibility : "hidden"}); 
     }); 
     $("#exit-add").on("click", function() {
         $(".add-container").css({visibility : "hidden"}); 
+    });
+    $("#filter").on("click", () => {
+        $(".filter-container").css({visibility : "visible"});
+        $(".add-container").css({visibility : "hidden"});
+        $(".user-container").css({visibility : "hidden"}); 
+        $(".info-container").css({visibility : "hidden"}); 
+    }); 
+    $("#exit-filter").on("click", function() {
+        $(".filter-container").css({visibility : "hidden"}); 
     });
 });
 

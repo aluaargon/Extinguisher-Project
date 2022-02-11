917ada406e56d99747a46b0f0d9af744081a9cd9
aaron <alurui1562@ieselcaminas.org>
2022-02-11 13:06:13 +0100

    añadido usuarios tocando el user container

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,26 @@
-$("#demo").on("click", function() {
-    console.log("lol");
-    $.ajax({
-        type: "GET",
-        url : "/demo"
-    }).done(() => alert("done"))
-    .fail((request) => alert("something went wrong " + request.status));
-   });
+$(document).ready(function () {  
+    $("#log-in").click(function() {
+        $.ajax({
+            type: "GET", 
+            url : "/login" 
+        }).done((response) => {
+           $(".user-container").html(response);
+        }) 
+    });
+
+    $("#register").click(function() {
+        $.ajax({
+            type: "GET", 
+            url : "/register" 
+        }).done((response) => {
+           $(".user-container").html(response);
+        }) 
+    });
+
+    $("#log-out").click(function() {
+        $.ajax({
+            type: "GET", 
+            url : "/logout" 
+        }).done(() => location.reload())
+    });
+});
